b1a1d66e92cebba7d894dee197be7a3bb584a877
Wellington-Roveder <rovederwellington@gmail.com>
2026-07-31 16:54:09 -0300

    !feat: adding the execution queue thread

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import queue
 import time
 
 import pyttsx3
@@ -7,7 +8,10 @@ from googletrans import Translator
 if __name__ == "__main__":
     translator = Translator()
     text_historic = []
+
     engine = pyttsx3.init()
+    ###adding a queue for translations for more otimization
+    translation_queue = queue.Queue()
 
     def callback_audio(recognizer, audio):
         try:
@@ -23,25 +27,35 @@ if __name__ == "__main__":
         if len(text_historic) > 3:
             text_historic.pop(0)
 
-        complete_context = " ".join(text_historic)
         try:
-            translation = translator.translate(complete_context, dest="pt").text
+            """Instead of saying it here, we put the text in the queue."""
+            translation = translator.translate(text_en, dest="pt").text
             print(f"🇧🇷 Tradução: {translation}\n" + "-" * 40)
+
+            translation_queue.put(translation)
         except Exception as e:  # noqa: BLE001
             print(e)
             return
-        engine.say(translation)
-        engine.runAndWait()
 
     r = sr.Recognizer()
     mic = sr.Microphone()
 
     with mic as source:
+        print("adjusting ambient noise...")
         r.adjust_for_ambient_noise(source, duration=1)
+        print("READ!")
 
     stop_listening = r.listen_in_background(mic, callback_audio)
+
     try:
         while True:
-            time.sleep(0.5)
+            if not translation_queue.empty():
+                """Check if there is anything in the queue blocking recognition."""
+                text_to_speak = translation_queue.get()
+                engine.say(text_to_speak)
+                engine.runAndWait()
+
+            time.sleep(0.1)
     except KeyboardInterrupt:
+        print("\nEnding...")
         stop_listening(wait_for_stop=False)
